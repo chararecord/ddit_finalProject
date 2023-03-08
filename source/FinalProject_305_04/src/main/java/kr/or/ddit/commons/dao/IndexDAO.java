b591e083382a4e90a5bd26dd4c0e7d8189820ef0
chararecord <any_think@naver.com>
2023-03-08 20:56:42 +0900

    230308 수

---
+++ source/FinalProject_305_04/src/main/java/kr/or/ddit/commons/dao/IndexDAO.java
@@ -0,0 +1,21 @@
+package kr.or.ddit.commons.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author 민경진
+ * @Since 2023. 3. 8.
+ * <pre>
+ *
+ * ======[[개정이력(Modification Information)]]======
+ *   수정일                    수정자                        수정내용
+ * --------     --------    -----------------------
+ * 2023. 3. 8.    민경진        		생성
+ *
+ * Copyright (c) 2023 by DDIT All right reserved
+ * </pre>
+ */
+@Mapper
+public interface IndexDAO {
+
+}
---
+++ source/FinalProject_305_04/src/main/java/kr/or/ddit/commons/service/IndexService.java
@@ -0,0 +1,28 @@
+package kr.or.ddit.commons.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import kr.or.ddit.campus.notice.dao.NoticeDAO;
+import kr.or.ddit.vo.NoticeVO;
+import kr.or.ddit.vo.PagingVO;
+
+/**
+ * @author 민경진
+ * @Since 2023. 3. 8.
+ * <pre>
+ *
+ * ======[[개정이력(Modification Information)]]======
+ *   수정일                    수정자                        수정내용
+ * --------     --------    -----------------------
+ * 2023. 3. 8.    민경진        		생성
+ *
+ * Copyright (c) 2023 by DDIT All right reserved
+ * </pre>
+ */
+public interface IndexService {
+	
+}
